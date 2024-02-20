a6a8ee91f21103cce04c579d38d8a062632dcc57
Yared Mesele <yaredbest81@gmail.com>
2024-02-20 11:40:43 +0300

    new email

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import LoginCard from "@/components/LoginCard";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen mx-2">
+    <main className="flex items-center justify-center min-h-screen mx-2 s">
       <LoginCard />
     </main>
   );
